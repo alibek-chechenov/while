6175e95c5566b3b73d664f886c8ea98df442c329
Alibek -asdf <143384474+alibek-chechenov@users.noreply.github.com>
2023-09-07 20:51:36 +0300

    first commit

---
+++ Blaknot__fail__script.js
@@ -0,0 +1,116 @@
+/*STROGİY RECİM = USE STRİNT
+"use strint"; TRUE
+console.log('ucim js');
+*/
+
+/*NE ZABİVAİ UKARACİVAT GLİNİE TEXTİ!
+document.getElementById('anti_text').innerHTML = 'ocen glinniy text kotoriy nado razorvat'; FALSE
+
+document.getElementById('text').innerHTML = 
+'ukoroceniy text!'; TRUE
+*/
+
+/*NE ZABIVAİ DOBOVLAT OTSTUPI DLA CİTABİLNOSTİ TESTA! = 'TEXT'
+let anti_text='anti_text';FALSE
+let text = 'text';TRUE
+alert( text + "dopolnitelniy text");
+*/
+
+/*NE ZABİVAY STAVİT TOCKA ZAPETOY! = ;
+console.log('ucim1 js') FALSE
+['ucim2', 'js'].forEach(alert)
+
+console.log('ucim1 js'); TRUE
+['ucim2', 'js'].forEach(alert);*/
+
+/*
+//zadanie N%1 True
+console.log('ucim js'); 
+
+//zadanie N%2 True
+console.log('js');
+console.log('ucim');
+
+//zadanie N%3 False
+console.log('ucim') console.log('js')
+
+//zadanie N%4 True
+console.log('ucim');
+console.log('js');
+*/
+
+    /*Адаптивные изображения
+    <picture>
+        <source srcset="lubaya/silka1" type="lubaya/silka1" media="(lubaya/silka1)">    
+        <source srcset="lubaya/silka2" type="lubaya/silka2" media="(lubaya/silka2)">
+        <source srcset="lubaya/silka3" type="lubaya/silka3" media="(lubaya/silka3)">
+    </picture>*/
+
+
+
+    /*govno!!!!
+@import "nullsryleçscss";
+
+.wrapper{
+    height: 100%;
+    overflow: hidden;
+    min-height: 100%;
+    padding: 50px;
+}
+
+body{
+    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+}
+.block__row{
+    border: 20px solid #ece89d;
+    margin: 0px 0px 20px 0px;
+} 
+.block__column{
+    border: 20px solid #5e5337;
+}
+.block__item{
+    background-color:red;
+    padding: 50px;
+    font-size: 50px;
+    color: tan;
+    font-weight: 700;
+}
+.block__list{
+    display: flex;
+    align-items: center;
+    li{margin: 0px 15px 0px 0px;}
+    &:last-child{margin: 0px 0px 0px 0px;}
+}
+.block__info{
+    position: relative;
+    padding: 0px 0px 0px 0px;
+
+}/*govno!!!
+    /*<div class="wrapper">
+     <div class="block">
+      <div class="block__row">
+       <div class="block__column">
+        <div class="block__item">1</div>
+       </div>
+         <div class="block__column">
+            <div class="block__item">2</div>
+         </div>
+          <div class="block__column">
+            <div class="block__item">3</div>
+          </div>*/
+
+/*p-начало 
+let i = 0;
+ 
+for (;i < 3; i++){
+    alert( i )
+}
+p-шаг
+let i = 0;
+for (; i < 3;){
+    alert( i++ );
+
+    for (;;){
+         // будет выполняться вечн
+    }
+}*/
